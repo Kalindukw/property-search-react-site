050fb8b80c3de694b2d873a46a3512b4d1525751
Kalindukw <108806927+Kalindukw@users.noreply.github.com>
2025-01-07 11:07:58 +0530

    Updated:Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,18 +3,23 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary mb-4">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
       <div className="container">
-        <Link className="navbar-brand" to="/">Real Estate App</Link>
-        <button 
-          className="navbar-toggler" 
-          type="button" 
-          data-bs-toggle="collapse" 
+        <Link className="navbar-brand" to="/">
+          <span className="brand-dream">Dream</span>
+          <span className="brand-key">Key</span>
+          {" "}
+          <span className="brand-realty">Realty</span>
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
           data-bs-target="#navbarNav"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <div className= " justify-content-end collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
               <Link className="nav-link" to="/">Home</Link>
@@ -25,6 +30,21 @@ function Navbar() {
             <li className="nav-item">
               <Link className="nav-link" to="/favorites">Favorites</Link>
             </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">About Us</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact">Contact</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/login">Login</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link btn btn-primary sign-up-btn" to="/signup">
+              Sign Up
+              </Link>
+          </li>
+
           </ul>
         </div>
       </div>
